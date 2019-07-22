232c271b3d7951ee55cc13f788cae158a6bcc10a
leyan95 <18231926271@163.com>
2019-07-22 19:33:02 +0800

    update

--- lib/hunter/widgets/table_list.dart
+++ lib/hunter/widgets/table_list.dart
@@ -14,6 +14,60 @@ class TableList<T> extends StatefulWidget {
 class TableListState extends State<TableList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: List<Widget>.generate(
+        20,
+        (index) => Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.purpleAccent,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.tealAccent,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.teal,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.orange,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.deepPurpleAccent,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: (index % 2) == 0 ? Colors.blue : Colors.deepOrangeAccent,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
