42d40bfc3e18c177af31a4d7215e8c11a505b403
leyan95 <18231926271@163.com>
2019-07-26 07:19:34 +0800

    update

--- lib/hunter/widgets/table_list.dart
+++ lib/hunter/widgets/table_list.dart
@@ -1,16 +1,47 @@
+//import 'dart:mirrors';
+
 import 'package:flutter_web/cupertino.dart';
 import 'package:flutter_web/material.dart';
 
 TextStyle headStyle = TextStyle(fontSize: 25, fontFamily: 'FangZhengCuKaiJianTi');
 List<TableItem> tableItems = [
-  TableItem(columnName: '编号', align: TableAlign.left),
-  TableItem(columnName: '名称', align: TableAlign.left),
-  TableItem(columnName: '库房', align: TableAlign.left),
-  TableItem(columnName: '生产日期', align: TableAlign.left),
-  TableItem(columnName: '单价', align: TableAlign.left),
-  TableItem(columnName: '金额', align: TableAlign.left),
-  TableItem(columnName: '状态', align: TableAlign.left),
-  TableItem(columnName: '备注', align: TableAlign.left)
+  TableItem(bindId: 'id', columnName: '编号', align: TableAlign.left),
+  TableItem(bindId: 'name', columnName: '名称', align: TableAlign.left),
+  TableItem(bindId: 'warehouse', columnName: '库房', align: TableAlign.left),
+  TableItem(bindId: 'date', columnName: '生产日期', align: TableAlign.left),
+  TableItem(bindId: 'price', columnName: '单价', align: TableAlign.right),
+  TableItem(bindId: 'money', columnName: '金额', align: TableAlign.right),
+  TableItem(bindId: 'status', columnName: '状态', align: TableAlign.left),
+  TableItem(bindId: 'memo', columnName: '备注', align: TableAlign.left)
+];
+
+class TestModel {
+  final String id;
+  final String name;
+  final String warehouse;
+  final DateTime date;
+  final double price;
+  final double money;
+  final String status;
+  final String memo;
+
+  TestModel({this.id, this.name, this.warehouse, this.date, this.price, this.money, this.status, this.memo});
+}
+
+List<TestModel> dataList = [
+  TestModel(id: '001', name: '一次性血袋-001', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '002', name: '一次性血袋-002', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '003', name: '一次性血袋-003', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '004', name: '一次性血袋-004', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '005', name: '一次性血袋-005', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '006', name: '一次性血袋-006', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '007', name: '一次性血袋-007', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '008', name: '一次性血袋-008', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '009', name: '一次性血袋-009', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '010', name: '一次性血袋-010', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '011', name: '一次性血袋-011', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '012', name: '一次性血袋-012', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
+  TestModel(id: '013', name: '一次性血袋-013', warehouse: '中心库房', date: DateTime.now(), price: 200.0, money: 2000.0, status: '入库', memo: '备注一下'),
 ];
 
 class ScrollTable<T> extends StatefulWidget {
@@ -28,6 +59,11 @@ class ScrollTableState extends State<ScrollTable> {
 
   @override
   Widget build(BuildContext context) {
+//    InstanceMirror myClassInstanceMirror = reflect(dataList[0]);
+//    ClassMirror MyClassMirror = myClassInstanceMirror.type;
+//    for (var k in MyClassMirror.declarations.keys) {
+//      print(MirrorSystem.getName(k));
+//    }
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Column(
@@ -40,12 +76,12 @@ class ScrollTableState extends State<ScrollTable> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: tableItems
                       .map((item) => Container(
-                            width: item.width,
-                            child: Text(
-                              item.columnName,
-                              style: headStyle,
-                            ),
-                          ))
+                    width: item.width,
+                    child: Text(
+                      item.columnName,
+                      style: headStyle,
+                    ),
+                  ))
                       .toList(),
                 ),
               ),
@@ -55,9 +91,10 @@ class ScrollTableState extends State<ScrollTable> {
             child: SizedBox(
               width: _tableWidth,
               child: ListView(
+                // TODO
                 children: List<Widget>.generate(
                   20,
-                  (index) => Row(
+                      (index) => Row(
                     children: <Widget>[
                       Container(
                         height: 40,
@@ -83,12 +120,13 @@ class ScrollTableState extends State<ScrollTable> {
 }
 
 class TableItem {
+  final String bindId;
   final String columnName;
   final double width;
   final String align;
   final String format;
 
-  TableItem({this.columnName, this.width = 150, this.align, this.format});
+  TableItem({@required this.bindId, this.columnName, this.width = 150, this.align, this.format});
 }
 
 class TableAlign {
